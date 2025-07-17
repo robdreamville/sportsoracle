93c0f2dac384a8d7c013f80c8934221101f17ed6
robdreamville <1997believe@gmail.com>
2025-07-17 13:13:59 -0400

    changes to pipeline

--- src/embed_cluster.py
+++ src/embed_cluster.py
@@ -151,7 +151,8 @@ def cluster_embeddings(embeddings, texts=None, method="bertopic", hdbscan_min_cl
         model = BERTopic(min_topic_size=bertopic_min_topic_size, vectorizer_model=vectorizer_model)
         labels, _ = model.fit_transform(texts, embeddings)
         # Reduce topics to merge highly similar ones and eliminate redundancies
-        labels, model = model.reduce_topics(texts, nr_topics="auto")
+        model = model.reduce_topics(texts, nr_topics="auto")
+        labels = model.topics_
         return labels, model
 
     else:
